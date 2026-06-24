6aac46496c27c89b4dfb48853a2ba54cfe642f7a
seodongheon <seodongheon6@gmail.com>
2026-06-24 22:33:21 +0900

    [Passed] 길이 단위 변환하기 (94ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        double ft = 30.48;
+        int mi = 160934;
+        System.out.printf("9.2ft = %.1fcm\n", ft * 9.2);
+        
+        System.out.printf("1.3mi = %.1fcm", mi * 1.3);
+    }
+}
