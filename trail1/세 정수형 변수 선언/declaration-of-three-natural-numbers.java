fe2791118369a6659e944db89389807412b4b05e
seodongheon <seodongheon6@gmail.com>
2026-06-24 22:25:08 +0900

    [Passed] 세 정수형 변수 선언 (94ms, 9MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7, b = 23, c = 30;
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
