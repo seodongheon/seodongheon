8714de7e98dc98501cde5d0684b998b884521ad8
seodongheon <seodongheon6@gmail.com>
2026-06-24 22:22:31 +0900

    [Passed] 정수 선언하고 뺄셈 (91ms, 9MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int num1 = 97;
+        int num2 = 13;
+        System.out.print(num1 + " - " + num2 + " = " + (num1-num2));
+    }
+}
