dbc481e3c9bf203c1a265dd26b418acb701d18c7
seodongheon <seodongheon6@gmail.com>
2026-06-24 10:02:14 +0900

    [Passed] 특정 원소들의 합 (143ms, 10MB)

---
+++ trail0/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n[][] = new int[4][4];
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                n[i][j] = sc.nextInt();
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < i+1; j++){
+                sum += n[i][j];
+            }
+        }   
+        System.out.println(sum);
+    }
+}
