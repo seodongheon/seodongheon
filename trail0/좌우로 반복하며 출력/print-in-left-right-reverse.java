6f6347ee2766703113fdf2f9dab457a13c5ec837
seodongheon <seodongheon6@gmail.com>
2026-06-24 10:10:44 +0900

    [Passed] 좌우로 반복하며 출력 (126ms, 10MB)

---
+++ trail0/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                for(int j = 1; j <= n; j++){
+                    System.out.print(j);
+                }
+            }else{
+                for(int k = n; k >= 1; k--){
+                    System.out.print(k);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
