cc1d248fb40adc0b2581aba15bd3b2cd4069941b
seodongheon <seodongheon6@gmail.com>
2026-06-26 16:50:33 +0900

    [Passed] 날짜 변경하여 출력 2 (130ms, 10MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] str = s.split("-");
+        System.out.println(str[2] + "." + str[0] + "." + str[1]);
+
+    }
+}
