b39a22d8d85659755b6af0fe7b3296e87179a047
seodongheon <seodongheon6@gmail.com>
2026-06-27 13:51:12 +0900

    [Passed] 청소당번 정하기 (135ms, 10MB)

---
+++ trail1/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int cnt2 = 0;
+        int cnt3 = 0;
+        int cnt12 = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+
+            if(i % 12 == 0) cnt12++;
+            else if(i % 3 == 0) cnt3++;
+            else if(i % 2 == 0) cnt2++;
+        }
+        System.out.print(cnt2 + " " + cnt3 + " " + cnt12);
+    }
+}
