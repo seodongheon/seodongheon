1f77dfe5df6a89b761f1112cbf7c3aeac7662390
seodongheon <seodongheon6@gmail.com>
2026-06-28 22:48:45 +0900

    [Passed] 조건에 따른 연산 (132ms, 10MB)

---
+++ trail1/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true){
+            if(n % 2 == 0){
+                n = 3 * n + 1;
+                cnt++;
+                if(n>=1000) break;
+            }else{
+                n = 2 * n + 2;
+                cnt++;
+                if(n>=1000) break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
